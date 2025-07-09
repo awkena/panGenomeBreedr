a5db83f74824a59fc29d49ddce3ddf81b698ba3f
Israel-Tetteh <israeltetteh715@gmail.com>
2025-07-09 09:15:29 +0000

    app config updates

--- R/app_config.R
+++ R/app_config.R
@@ -1,40 +1,37 @@
 #' Access files in the current app
 #'
 #' NOTE: If you manually change your package name in the DESCRIPTION,
-#' don't forget to change it here too, and in the config file.
-#' For a safer name change mechanism, use the `golem::set_golem_name()` function.
+#' don't forget to update it here and in the config file.
+#' For a safer renaming mechanism, use `golem::set_golem_name()`.
 #'
-#' @param ... character vectors, specifying subdirectory and file(s)
-#' within your package. The default, none, returns the root of the app.
+#' @param ... character vectors specifying subdirectory and file(s)
+#' within your package. Default (none) returns the root of the app.
 #'
 #' @noRd
 app_sys <- function(...) {
-  options(shiny.maxRequestSize = 500 * 1024^2) # extend file upload size
+  options(shiny.maxRequestSize = 500 * 1024^2)  # Extend file upload size to 500 MB
   system.file(..., package = "panGenomeBreedr")
 }
+
 
 
 #' Read App Config
 #'
 #' @param value Value to retrieve from the config file.
-#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE.
-#' If unset, "default".
-#' @param use_parent Logical, scan the parent directory for config file.
-#' @param file Location of the config file
+#' @param config GOLEM_CONFIG_ACTIVE value. If unset, falls back to R_CONFIG_ACTIVE.
+#' If unset, defaults to "default".
+#' @param use_parent Logical. If TRUE, scan parent directory for config file.
+#' @param file Path to config file. Defaults to internal "golem-config.yml".
 #'
 #' @noRd
 get_golem_config <- function(
-  value,
-  config = Sys.getenv(
-    "GOLEM_CONFIG_ACTIVE",
-    Sys.getenv(
-      "R_CONFIG_ACTIVE",
-      "default"
-    )
-  ),
-  use_parent = TRUE,
-  # Modify this if your config file is somewhere else
-  file = app_sys("golem-config.yml")
+    value,
+    config = Sys.getenv(
+      "GOLEM_CONFIG_ACTIVE",
+      Sys.getenv("R_CONFIG_ACTIVE", "default")
+    ),
+    use_parent = TRUE,
+    file = app_sys("golem-config.yml")
 ) {
   config::get(
     value = value,
