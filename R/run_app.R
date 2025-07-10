852d32bb3d7a8b56663d17d9939ed4ec566bbf73
Israel-Tetteh <israeltetteh715@gmail.com>
2025-07-10 07:27:51 +0000

    run app updates

--- R/run_app.R
+++ R/run_app.R
@@ -9,11 +9,40 @@
 #' @importFrom golem with_golem_options
 run_app <- function(
     onStart = NULL,
+    auto_install = FALSE,
     options = list(),
     enableBookmarking = NULL,
     uiPattern = "/",
     ...) {
-  # I will add a humor message of the user to read here.
+  # Check all suggested packages at the start
+  suggested_packages <- c(
+    "DT", "data.table", "fontawesome", "openxlsx", "reactable",
+    "readxl", "shinyWidgets", "shinyalert", "shinybusy", "shinyjs",
+    "stringr", "vcfR", "writexl"
+  )
+
+  missing_packages <- suggested_packages[!sapply(suggested_packages,
+                                                 requireNamespace,
+                                                 quietly = TRUE
+  )]
+
+  if (length(missing_packages) > 0) {
+    if (auto_install) {
+      message("Installing missing packages: ", paste(missing_packages, collapse = ", "))
+      install.packages(missing_packages)
+    } else {
+      stop(
+        sprintf("Missing packages: %s\nInstall with: install.packages(c(%s))",
+                paste(missing_packages, collapse = ", "),
+                paste(sprintf("'%s'", missing_packages), collapse = ", ")),
+        call. = FALSE
+      )
+    }
+  }
+
+  # A humour message
+   cat('From the Green Evolution Project to the World!')
+
 
   with_golem_options(
     app = shinyApp(
